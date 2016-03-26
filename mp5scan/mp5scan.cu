f387cc9b33485b776cb594a297a83863566267d0
SunnyBingoMe <SunnyBingoMe@gmail.com>
2016-03-26 19:37:16 +0100

    partly finished. finished main(). modified release folder

--- mp5scan/mp5scan.cu
+++ mp5scan/mp5scan.cu
@@ -26,62 +26,64 @@ void __syncthreads();
 #define blockId_rowIndex blockIdx.y
 
 /* specific define for this project */
-#define BLOCK_SIZE      512
+#define BLOCK_SIZE      512 // you can modify this
+#define NrItemEachThred 2   // do not modify this
 
 __global__ 
-void scan(float * input, float * output, int len) {
-    //@@ Modify the body of this function to complete the functionality of
-    //@@ the scan on the device
-    //@@ You may need multiple kernel calls; write your kernels before this
-    //@@ function and call them from here
+void scan_global(float * pd_input, float * pd_output, int numElements) {
+    //@@ Modify the body of this function to complete the functionality of the scan on the device
+    //@@ You may need multiple kernel calls; write your kernels before this function and call them from here
 }
 
 int main(int argc, char ** argv) {
     wbArg_t args;
     float * hostInput; // The input 1D list
     float * hostOutput; // The output list
-    float * deviceInput;
-    float * deviceOutput;
+    float * pd_input;
+    float * pd_output;
     int numElements; // number of elements in the list
+    int sizeByte;
 
     args = wbArg_read(argc, argv);
 
     wbTime_start(Generic, "Importing data and creating memory on host");
     hostInput = (float *)wbImport(wbArg_getInputFile(args, 0), &numElements);
-    hostOutput = (float*)malloc(numElements * sizeof(float));
+    sizeByte = numElements * sizeof(float);
+    hostOutput = (float*)malloc(sizeByte);
     wbTime_stop(Generic, "Importing data and creating memory on host");
 
     wbLog(TRACE, "The number of input elements in the input is ", numElements);
 
     wbTime_start(GPU, "Allocating GPU memory.");
-    wbCheck(cudaMalloc((void**)&deviceInput, numElements*sizeof(float)));
-    wbCheck(cudaMalloc((void**)&deviceOutput, numElements*sizeof(float)));
+    wbCheck(cudaMalloc((void**)&pd_input, sizeByte));
+    wbCheck(cudaMalloc((void**)&pd_output, sizeByte));
     wbTime_stop(GPU, "Allocating GPU memory.");
 
     wbTime_start(GPU, "Clearing output memory.");
-    wbCheck(cudaMemset(deviceOutput, 0, numElements*sizeof(float)));
+    wbCheck(cudaMemset(pd_output, 0, sizeByte));
     wbTime_stop(GPU, "Clearing output memory.");
 
     wbTime_start(GPU, "Copying input memory to the GPU.");
-    wbCheck(cudaMemcpy(deviceInput, hostInput, numElements*sizeof(float), cudaMemcpyHostToDevice));
+    wbCheck(cudaMemcpy(pd_input, hostInput, sizeByte, cudaMemcpyHostToDevice));
     wbTime_stop(GPU, "Copying input memory to the GPU.");
 
     //@@ Initialize the grid and block dimensions here
+    dim3 dimBlock(BLOCK_SIZE, 1, 1);
+    dim3 dimGrid(numElements / (BLOCK_SIZE * NrItemEachThred), 1, 1);
 
     wbTime_start(Compute, "Performing CUDA computation");
-    //@@ Modify this to complete the functionality of the scan
-    //@@ on the deivce
-
+    //@@ Modify this to complete the functionality of the scan on the deivce
+    scan_global <<<dimGrid, dimBlock>>> (pd_input, pd_output, numElements);
     cudaDeviceSynchronize();
     wbTime_stop(Compute, "Performing CUDA computation");
 
     wbTime_start(Copy, "Copying output memory to the CPU");
-    wbCheck(cudaMemcpy(hostOutput, deviceOutput, numElements*sizeof(float), cudaMemcpyDeviceToHost));
+    wbCheck(cudaMemcpy(hostOutput, pd_output, sizeByte, cudaMemcpyDeviceToHost));
     wbTime_stop(Copy, "Copying output memory to the CPU");
 
     wbTime_start(GPU, "Freeing GPU Memory");
-    cudaFree(deviceInput);
-    cudaFree(deviceOutput);
+    cudaFree(pd_input);
+    cudaFree(pd_output);
     wbTime_stop(GPU, "Freeing GPU Memory");
 
     wbSolution(args, hostOutput, numElements);
